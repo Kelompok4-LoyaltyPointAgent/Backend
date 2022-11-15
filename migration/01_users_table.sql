6b72395695ab600c51d4f2f4c118ff67dc3677c4
Andika Bahari <andikabahari48@gmail.com>
2022-11-15 15:32:44 +0700

    Add users table migration

---
+++ migration/01_users_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3),
+  `updated_at` datetime(3),
+  `deleted_at` datetime(3),
+  `email` varchar(255),
+  `password` varchar(255),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_users_email` (`email`),
+  KEY `idx_users_deleted_at` (`deleted_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- +goose Down
+DROP TABLE IF EXISTS `users`;
