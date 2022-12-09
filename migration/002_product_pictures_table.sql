a23a7b29728ce1764f36ed2a474b9607282aa109
Andika Bahari <andikabahari48@gmail.com>
2022-12-09 12:59:35 +0700

    Fix icon field

--- migration/002_product_pictures_table.sql
+++ migration/002_product_pictures_table.sql
@@ -4,14 +4,11 @@ CREATE TABLE `product_pictures` (
   `created_at` datetime(3),
   `updated_at` datetime(3),
   `deleted_at` datetime(3),
-  `icon_id` varchar(255),
   `url` text,
   `name` varchar(255),
   `type` varchar(255),
   PRIMARY KEY (`id`),
-  KEY `idx_product_pictures_deleted_at` (`deleted_at`),
-  KEY `idx_product_pictures_icon` (`icon_id`),
-  CONSTRAINT `idx_product_pictures_icon` FOREIGN KEY (`icon_id`) REFERENCES `product_pictures` (`icon_id`)
+  KEY `idx_product_pictures_deleted_at` (`deleted_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- +goose Down
