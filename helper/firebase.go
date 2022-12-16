b7fdcb449b49e8667ff5f656de25a62b2036cd6e
j4ceu <sutan.raihan@ui.ac.id>
2022-12-17 00:58:49 +0700

    fix : linting

--- helper/firebase.go
+++ helper/firebase.go
@@ -19,19 +19,6 @@ import (
 
 var App *firebase.App
 
-type serviceAccountKey struct {
-	Type                    string `json:"type"`
-	ProjectID               string `json:"project_id"`
-	PrivateKeyID            string `json:"private_key_id"`
-	PrivateKey              string `json:"private_key"`
-	ClientEmail             string `json:"client_email"`
-	ClientID                string `json:"client_id"`
-	AuthURI                 string `json:"auth_uri"`
-	TokenURI                string `json:"token_uri"`
-	AuthProviderX509CertURL string `json:"auth_provider_x509_cert_url"`
-	ClientX509CertURL       string `json:"client_x509_cert_url"`
-}
-
 func InitAppFirebase() {
 	fmt.Println("Init firebase app")
 	firebaseConfig := config.LoadFirebaseConfig()
