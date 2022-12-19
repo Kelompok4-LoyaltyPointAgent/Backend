73edfe9058b6e37f88f1ff18a69f89e179718d5a
Andika Bahari <andikabahari48@gmail.com>
2022-12-19 20:49:43 +0700

    Remove validation for numbers.

--- dto/payload/product_payload.go
+++ dto/payload/product_payload.go
@@ -6,30 +6,30 @@ type ProductPayload struct {
 	Name           string                `form:"name" validate:"required,min=1,max=100"`
 	Description    string                `form:"description" validate:"required,min=1,max=500"`
 	Provider       string                `form:"provider" validate:"required,min=1,max=50"`
-	Price          uint                  `form:"price" validate:"required"`
-	PricePoints    uint                  `form:"price_points" validate:"required"`
-	RewardPoints   uint                  `form:"reward_points" validate:"required"`
-	Stock          uint                  `form:"stock" validate:"required"`
+	Price          uint                  `form:"price"`
+	PricePoints    uint                  `form:"price_points"`
+	RewardPoints   uint                  `form:"reward_points"`
+	Stock          uint                  `form:"stock"`
 	Recommended    *bool                 `form:"recommended"`
 	ProductPicture *multipart.FileHeader `form:"product_picture"`
 }
 
 type ProductWithCreditPayload struct {
 	ProductPayload
-	ActivePeriod int `form:"active_period" validate:"required"`
-	Amount       int `form:"amount" validate:"required"`
-	Call         int `form:"call" validate:"required"`
-	SMS          int `form:"sms" validate:"required"`
+	ActivePeriod int `form:"active_period"`
+	Amount       int `form:"amount"`
+	Call         int `form:"call"`
+	SMS          int `form:"sms"`
 }
 
 type ProductWithPackagesPayload struct {
 	ProductPayload
-	ActivePeriod       int     `form:"active_period" validate:"required"`
-	TotalInternet      float64 `form:"total_internet" validate:"required"`
-	MainInternet       float64 `form:"main_internet" validate:"required"`
-	NightInternet      float64 `form:"night_internet" validate:"required"`
-	SocialMedia        float64 `form:"social_media" validate:"required"`
-	Call               int     `form:"call" validate:"required"`
-	SMS                int     `form:"sms" validate:"required"`
+	ActivePeriod       int     `form:"active_period"`
+	TotalInternet      float64 `form:"total_internet"`
+	MainInternet       float64 `form:"main_internet"`
+	NightInternet      float64 `form:"night_internet"`
+	SocialMedia        float64 `form:"social_media"`
+	Call               int     `form:"call"`
+	SMS                int     `form:"sms"`
 	PackageDescription string  `form:"package_description" validate:"required,min=1,max=300"`
 }
